6694a95182c0bb88555655b0bc701736f845ff6f
exec zsh <wind10cloud@gmail.com>
2024-11-10 10:38:40 +0900

    initial commit

---
+++ src/main/java/dev/rslab/SimpleDiary/SimpleDiaryApplication.java
@@ -0,0 +1,13 @@
+package dev.rslab.SimpleDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rslab/SimpleDiary/SimpleDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rslab.SimpleDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
